29a7f54d90d7e80edd7aa4b9b76efc573228d8ed
Hannah Huang <hhuang@pinterest.com>
2020-04-15 21:30:31 -0700

    update

--- MyLeetCode/python/Merge Intervals.py
+++ MyLeetCode/python/Merge Intervals.py
@@ -3,26 +3,15 @@
 # Time Complexity: O(nlogn)
 # Space Complexity: O(n)
 ###
-
-# Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
-
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: List[Interval]
-        """
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if len(intervals) <= 1:
             return intervals
-        intervals.sort(key=lambda x: x.start)
-        re = []
-        for it in intervals: 
-            if not re or re[-1].end < it.start:
-                re.append(it)
+        intervals.sort(key=lambda e: e[0])
+        result = []
+        for e in intervals:
+            if not result or result[-1][1] < e[0]:
+                result.append(e)
             else:
-                re[-1].end = max(re[-1].end, it.end)
-        return re
+                result[-1][1] = max(result[-1][1], e[1])
+        return result
